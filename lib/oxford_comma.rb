ddecd70bcc2f557ca87b7b8441df0c9ad1a87a76
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 13:21:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     puts array
     elsif array.length == 2
-    puts array.
+    puts array.insert(-2)
   array_with_and = array.insert(-2, "and")
   string_no_last_element = array_with_and[0..-2].join(", ")
   string_last_element = array.pop
