1e5327e80b20e0a0fdf994ed4557ac3587ff3e15
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 13:09:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
-def oxford_comma(array_of_string_elements)
-
+def oxford_comma(array)
+  array_with_and = array.insert(-2, "and")
+  string_no_last_element = array_with_and[0..-2].join(", ")
+  string_last_element = array.pop
+  puts string_no_last_element + " #{string_last_element}"
 end
